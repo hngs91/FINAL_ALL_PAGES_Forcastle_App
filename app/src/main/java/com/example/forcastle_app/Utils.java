3d5cde35d8bfc91893d5e95b07f62d8041a866f2
hngs91 <hngsmith91@gmail.com>
2022-05-09 15:59:38 +0100

    Added in all 4 maps

--- app/src/main/java/com/example/forcastle_app/Utils.java
+++ app/src/main/java/com/example/forcastle_app/Utils.java
@@ -6,6 +6,6 @@ Code implemented by Eugenia Vuong
 public class Utils {
     // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
 
-    public static final  String EMAIL = "forcastle@gmail.com";
-    public static final String PASSWORD = "Forcastle22";
+    public static final  String EMAIL = "forcastleteam16@gmail.com";
+    public static final String PASSWORD = "For/castle@Team16";
 }
